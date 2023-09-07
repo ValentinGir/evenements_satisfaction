363b095a39a73204cc532ec8bfd6188406e96d93
ValentinGir <val.giraudon@gmail.com>
2023-09-07 16:17:52 -0400

    .

--- Cours4/index.php
+++ Cours4/index.php
@@ -14,13 +14,18 @@
     $username = "root";
     $password = "root";
     $dbname = "linge";
+    //$delete = false;
+    //$delete = (isset($_GET['delete']));
+    //$ajout = false;
+    //$ajout = (isset($_GET['ajout']));
+
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $conn->query('SET NAMES utf8');$sql = "SELECT * FROM tableimage";
+    $sql = "SELECT * FROM tableimage";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
     // output data of each row
@@ -35,14 +40,13 @@
                     <th scope="col">Description</th>
                     <th scope="col">Description2</th>
                     <th scope="col">Taille</th>
+                    <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
             
         <?php
         while($row = $result->fetch_assoc()) {
-            //echo "id: " . $row["id"]. " - Image: " . $row["image"]. " - Description: " . $row["description"]. 
-            //" - Description2: " . $row["description2"]. " - Taille: " . $row["taille"]. "<br>";
             ?>
             <tr>
                 <th scope="row"><?php echo $row["id"] ?></th>
@@ -50,6 +54,10 @@
                 <td><?php echo $row["description"] ?></td>
                 <td><?php echo $row["description2"] ?></td>
                 <td><?php echo $row["taille"] ?></td>
+                <td>
+                    <a href="modifier.php?id=<?php echo $row["id"] ?>">P</a><br>
+                    <a href="supprimer.php?id=<?php echo $row["id"] ?>">X</a>
+                </td>
             </tr>
             <?php
 
@@ -65,7 +73,34 @@
         <a href="ajouter.php">
             <button type="button" class="btn btn-primary btn-lg">Ajouter</button>
         </a>
-        
+
+        <?php
+        if(isset($_GET['delete']))
+        {
+            ?>
+            <div class="alert alert-danger mt-2 w-25 text-center" role="alert">
+            L'élément a été supprimé
+            </div>
+            <?php
+        }
+
+        if(isset($_GET['add']))
+        {
+            ?>
+            <div class="alert alert-success mt-2 w-25 text-center" role="alert">
+            L'élément a été ajouté
+            <?php
+        }
+
+        if(isset($_GET['edit']))
+        {
+            ?>
+            <div class="alert alert-warning mt-2 w-25 text-center" role="alert">
+            L'élément a été modifié
+            <?php
+        }
+        ?>
+        </div>
     </div>
 <script src="https://kit.fontawesome.com/97daa36ca6.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
