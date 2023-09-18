2d8d2158159240b9b15b8ad8e7cb8fa745eceb82
Valentin Giraudon <val.giraudon@gmail.com>
2023-09-18 14:41:10 -0400

    .

--- evenements_satisfaction/avisEtu.php
+++ evenements_satisfaction/avisEtu.php
@@ -11,30 +11,45 @@ session_start();
     <title>Avis</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            
-        </div>
-    </div>
     <?php
-        function insertAvis($avis) {
-            $servername = "localhost";
-            $username = "root";
-            $password = "root";
-            $dbname = "evenements_satisfaction";
-        
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
+        if($_SESSION["connexion"] == true)
+        { 
+            if(isset($_GET['id'])) {
+                $id=$_GET['id'];
             }
-            $id=$_GET['id'];
-            $sql = "SELECT * FROM evenements WHERE id=$id";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                
+            ?>
+            <div class="container">
+                <div class="row">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                        <button type="submit" name="submit" value="etuContent"><img src="img/content.PNG"></button>
+                        <button type="submit" name="submit" value="etuMid"><img src="img/mid.PNG"></button>
+                        <button type="submit" name="submit" value="etuMecontent"><img src="img/mecontent.PNG"></button>
+                    </form>
+                </div>
+            </div>
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "root";
+                $dbname = "evenements_satisfaction";
+            
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                if(isset($_POST['submit'])) {
+                    $submitValue = $_POST['submit'];
+                    $sql = "SELECT $submitValue FROM evenements WHERE id=$id";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        $nbAvis = $row[$submitValue];
+                        echo "<h1>$nbAvis</h1>";
+                    }
                 }
         }
-    ?>
+        else
+        header('Location: connexion.php');
+            ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
